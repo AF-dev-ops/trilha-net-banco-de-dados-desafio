e9ee90e13ca9b4a258392dc3623d34f30410aa5b
AF-dev-ops <alfredofernando.compras@gmail.com>
2024-09-23 19:55:04 -0300

    Create Solucao Desafio.sql

---
+++ Solucao Desafio.sql
@@ -0,0 +1,62 @@
+-- Consulta 1
+SELECT Nome, Ano
+  FROM Filmes;
+
+-- Consulta 2
+SELECT Nome, Ano, Duracao
+  FROM Filmes
+ ORDER BY Ano;
+
+-- Consulta 3
+SELECT Nome, Ano, Duracao
+  FROM Filmes
+ WHERE Nome = 'De Volta para o Futuro';
+
+-- Consulta 4
+SELECT Nome, Ano, Duracao
+  FROM Filmes
+ WHERE Ano = 1997;
+
+-- Consulta 5
+SELECT Nome, Ano, Duracao
+  FROM Filmes
+ WHERE Ano > 2000;
+
+-- Consulta 6
+SELECT Nome, Ano, Duracao
+  FROM Filmes
+ WHERE Duracao > 100 AND Duracao < 150
+ ORDER BY Duracao;
+
+-- Consulta 7
+SELECT Ano, COUNT(*) AS Quantidade
+  FROM Filmes
+ GROUP BY Ano
+ ORDER BY Quantidade DESC;
+
+-- Consulta 8
+SELECT Id, PrimeiroNome, UltimoNome, Genero
+  FROM Atores
+ WHERE Genero = 'M';
+
+-- Consulta 9
+SELECT Id, PrimeiroNome, UltimoNome, Genero
+  FROM Atores
+ WHERE Genero = 'F'
+ ORDER BY PrimeiroNome;
+
+-- Consulta 10
+SELECT Nome, Genero
+  FROM Filmes INNER JOIN FilmesGenero ON (Filmes.Id = FilmesGenero.IdFilme)
+              INNER JOIN Generos      ON (FilmesGenero.IdGenero = Generos.Id);
+
+-- Consulta 11
+SELECT Nome, Genero
+  FROM Filmes INNER JOIN FilmesGenero ON (Filmes.Id = FilmesGenero.IdFilme)
+              INNER JOIN Generos      ON (FilmesGenero.IdGenero = Generos.Id)
+ WHERE Genero = 'Mistério';
+
+-- Consulta 12
+SELECT Nome, PrimeiroNome, UltimoNome, Papel
+  FROM Filmes INNER JOIN ElencoFilme ON (Filmes.Id = ElencoFilme.IdFilme)
+              INNER JOIN Atores      ON (ElencoFilme.IdAtor = Atores.Id);
